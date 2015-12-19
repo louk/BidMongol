f694534cf70bfe1fbf753b7a807d140f14bad7f0
Naranbayar <naranbayar@gmail.com>
2015-12-19 14:29:57 -0800

    Tableview added

--- BidMongol/DriverViewController.swift
+++ BidMongol/DriverViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Parse
 
-class DriverViewController: UIViewController {
+class DriverViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var searchTableView: UISearchBar!
+    @IBOutlet weak var userTableView: UITableView!
+    
+    var mutableArray:NSMutableArray? = NSMutableArray()
+    
+    var myArray = ["Steve", "Bill", "Linus", "Bret"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.userTableView.delegate = self
+        self.userTableView.dataSource = self
+        
+        self.getUserList()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +34,53 @@ class DriverViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int)-> Int {
+        print("%d",self.mutableArray!.count)
+        return self.myArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let firstName : UILabel = cell.viewWithTag(1) as! UILabel;
+        let zipCode : UILabel = cell.viewWithTag(2) as! UILabel;
+       // firstName.text = self.myArray.objectAtIndex(indexPath.row) as? String
+        print(firstName.text)
+        print(self.mutableArray!.objectAtIndex(indexPath.row) as? String)
+        return cell
+        
+    }
+    
+    
+    
+    func getUserList(){
+   
+        let query:PFQuery=PFQuery(className: "_User");
+        
+            
+        query.findObjectsInBackgroundWithBlock {
+            (objects:[PFObject]?, error:NSError?) -> Void in
+            if !(error != nil) {
+                for(var i=0;i<objects!.count;i++){
+                    let object=objects![i] ;
+                    let name = object.objectForKey("username") as! String;
+                    
+                   // self.mutableArray!.addObject(name)
+            
+                    print(name)
+                    
+                }
+            }
+        }
+       
+        print(self.mutableArray)
+        userTableView.reloadData()
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
